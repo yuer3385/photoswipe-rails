09e929243e5b8c04a67d6fef9d61f61aec42793d
yuer3385 <qq5201314>
2018-04-02 17:53:24 +0800

    feat #修改文件目录

--- lib/photoswipe-rails/engine.rb
+++ lib/photoswipe-rails/engine.rb
@@ -5,7 +5,6 @@ module PhotoSwipe
       Rails.application.config.assets.precompile += %w{ photoswipe-ui-default.js }
       Rails.application.config.assets.precompile += %w{ photoswipe.scss }
       Rails.application.config.assets.precompile += %w( default-skin/default-skin.scss default-skin/default-skin.png default-skin/default-skin.svg  default-skin/preloader.gif )
-      Rails.application.config.assets.paths << root.join('app', 'assets')
     end
   end
 end
